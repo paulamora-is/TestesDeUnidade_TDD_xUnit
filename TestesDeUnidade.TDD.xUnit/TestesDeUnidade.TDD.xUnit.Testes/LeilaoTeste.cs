873b47c9b89ca161c38e20ddd1ee520a519dcdec
Paula Morais <pmorais78@gmail.com>
2021-09-12 03:54:42 -0300

    Criado teste com Theories

--- TestesDeUnidade.TDD.xUnit/TestesDeUnidade.TDD.xUnit.Testes/LeilaoTeste.cs
+++ TestesDeUnidade.TDD.xUnit/TestesDeUnidade.TDD.xUnit.Testes/LeilaoTeste.cs
@@ -5,46 +5,28 @@ namespace TestesDeUnidade.TDD.xUnit.Testes
 {
     public class LeilaoTeste
     {
-        [Fact]
-        public void TerminarPregao_UltimoLanceForMaiorValor()
+        [Theory]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData(1000, new double[] { 800, 500, 1000, 900 })]
+        [InlineData(800, new double[] { 800 })]
+        public void TerminarPregao_QuandoLancesForemValoresEsperados(double valorEsperado,
+            double[] valoresOfertados)
         {
             //Arrange
             var leilao = new Leilao("Vincent van Gogh");
-            var primeiroCliente = new Cliente("Maria", leilao);
-            var segundoCliente = new Cliente("João", leilao);
+            var cliente = new Cliente("cliente", leilao);
 
-            leilao.ReceberLance(primeiroCliente, 800);
-            leilao.ReceberLance(segundoCliente, 900);
-            leilao.ReceberLance(primeiroCliente, 1000);
-            leilao.ReceberLance(segundoCliente, 1200);
+            foreach (var valor in valoresOfertados)
+            {
+                leilao.ReceberLance(cliente, valor);
+            }
 
             //Act
             leilao.TerminarPregao();
 
             //Assert
-            var valorExperado = 1200;
             var valorObtido = leilao.Ganhador.Valor;
-            var clienteGanhador = segundoCliente;
-            Assert.Equal(valorExperado, valorObtido);
-            Assert.Equal(clienteGanhador, leilao.Ganhador.Cliente);
-        }
-
-        [Fact]
-        public void TerminarPregao_QuandoExistirUnicoLance()
-        {
-            //Arrange
-            var leilao = new Leilao("Vincent van Gogh");
-            var unicoCliente = new Cliente("Maria", leilao);
-
-            leilao.ReceberLance(unicoCliente, 800);
-
-            //Act
-            leilao.TerminarPregao();
-
-            //Assert
-            var valorExperado = 800;
-            var valorObtido = leilao.Ganhador.Valor;
-            Assert.Equal(valorExperado, valorObtido);
+            Assert.Equal(valorEsperado, valorObtido);
         }
 
         [Fact]
